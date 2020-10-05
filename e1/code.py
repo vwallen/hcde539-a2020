68f1822a6a90bc1e183582ddfa6739d47d497d14
Victor W Allen <vwallen@antlab.net>
2020-10-05 15:12:04 -0700

    Adds color theme toggling via A and B buttons

--- e1/code.py
+++ e1/code.py
@@ -1,22 +1,31 @@
 import math
 import time
 from adafruit_circuitplayground import cp
+from collections import namedtuple
 
-# Color presets
-OFF = (0, 0, 0)
-COLOR_A = (0, 0, 128) # main pointer color
-COLOR_B = (1, 0, 1) # peripheral pointer color
+# Create a collection of color themes
+ColorTheme = namedtuple('Theme', ['base', 'primary', 'secondary'])
+themes = [
+    ColorTheme((0, 0, 0), (0, 255, 0), (8, 8, 0)),
+    ColorTheme((0, 0, 0), (0, 0, 255), (4, 0, 4)),
+    ColorTheme((0, 0, 0), (255, 0, 0), (64, 32, 0)),
+    ColorTheme((1, 2, 1), (0, 255, 0), (8, 12, 0)),
+    ColorTheme((1, 1, 1), (0, 0, 255), (16, 4, 16)),
+    ColorTheme((2, 1, 0), (255, 0, 0), (64, 32, 0)),
+]
+theme = themes[0]
 
 # Map positions of the compass to indexes of
 # cp.pixels, including positions with no
-# LED present (90° abd 120°)
+# LED present (90° and 120°)
 positions = [7, 8, 9, None, 0, 1, 2, 3, 4, None, 5, 6]
 num_positions = len(positions)
 
 FULL_CIRCLE = math.pi * 2
 
+button_a_up = True
+button_b_up = True
 
-n = 0
 while True:
     x, y, z = cp.acceleration
 
@@ -29,16 +38,32 @@ while True:
     index_a = positions[(position + 1) % num_positions] # use modulo to wrap
     index_b = positions[position - 1]
 
-    cp.pixels.fill(OFF)
+    # Use buttones to cycle through color themes
+    # Trigger action on release, to represent "clicks"
+    if cp.button_a:
+        button_a_up = False
+    else:
+        if not button_a_up:
+            theme = themes[(themes.index(theme) + 1) % len(themes)]
+        button_a_up = True
+
+    if cp.button_b:
+        button_b_up = False
+    else:
+        if not button_b_up:
+            theme = themes[(themes.index(theme) - 1)]
+        button_b_up = True
+
+    cp.pixels.fill(theme.base)
 
     # Don't change the color of non-existent LEDs
     if index is not None:
-        cp.pixels[index] = COLOR_A
+        cp.pixels[index] = theme.primary
 
     if index_a is not None:
-        cp.pixels[index_a] = COLOR_B
+        cp.pixels[index_a] = theme.secondary
 
     if index_b is not None:
-        cp.pixels[index_b] = COLOR_B
+        cp.pixels[index_b] = theme.secondary
 
     time.sleep(0.066) # 66ms = ~15fps
